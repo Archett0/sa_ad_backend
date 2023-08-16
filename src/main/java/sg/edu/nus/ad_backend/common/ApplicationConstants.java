f61d1ccc6eb753128579242c0e9ad449a6bf15b8
Cooper <archett0@outlook.com>
2023-08-16 16:02:27 +0800

    add constants

---
+++ src/main/java/sg/edu/nus/ad_backend/common/ApplicationConstants.java
@@ -0,0 +1,11 @@
+package sg.edu.nus.ad_backend.common;
+
+public class ApplicationConstants {
+    public static final Integer APPLICATION_PENDING = 0;
+    public static final Integer APPLICATION_APPROVED = 1;
+    public static final Integer APPLICATION_REJECTED = 2;
+    public static final Integer APPLICATION_READY_FOR_COLLECTION = 3;
+    public static final Integer APPLICATION_COMPLETED = 4;
+    public static final Integer APPLICATION_CANCELLED = 5;
+    public static final Integer APPLICATION_DISABLED = 6;
+}
--- src/main/java/sg/edu/nus/ad_backend/common/BookConstants.java
+++ src/main/java/sg/edu/nus/ad_backend/common/BookConstants.java
@@ -3,4 +3,9 @@ package sg.edu.nus.ad_backend.common;
 public class BookConstants {
     public static final Integer BOOK_PENDING = 0;
     public static final Integer BOOK_DEPOSITED = 1;
+    public static final Integer BOOK_AVAILABLE = 2;
+    public static final Integer BOOK_RESERVED = 3;
+    public static final Integer BOOK_UNAVAILABLE = 4;
+    public static final Integer BOOK_REJECTED = 5;
+    public static final Integer BOOK_DISABLED = 6;
 }
