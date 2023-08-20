f15020c4fe8c35140df2ffec1e9f56be321a8710
Cooper <archett0@outlook.com>
2023-08-20 13:13:00 +0800

    drop redundant tables

--- src/main/java/sg/edu/nus/ad_backend/repository/MemberCreditRepository.java
+++
@@ -1,7 +0,0 @@
-package sg.edu.nus.ad_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sg.edu.nus.ad_backend.model.MemberCredit;
-
-public interface MemberCreditRepository extends JpaRepository<MemberCredit, Long> {
-}
--- src/main/java/sg/edu/nus/ad_backend/repository/ReviewLogRepository.java
+++
@@ -1,7 +0,0 @@
-package sg.edu.nus.ad_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sg.edu.nus.ad_backend.model.ReviewLog;
-
-public interface ReviewLogRepository extends JpaRepository<ReviewLog, Long> {
-}
